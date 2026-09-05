f4479efa9aa12c1a5849bd30c760dca4c562c29b
Sierra-001A <48867097+Sierra-001@users.noreply.github.com>
2026-09-05 20:28:02 +0400

    Added Additional line

--- program.java
+++ program.java
@@ -1,6 +1,11 @@
 import java.io.*;
 class program{
     public static void main(String arg[]){
-        System.out.println("My name is Mr. Joe");
+        System.out.println("Name : Mr. My Name");
+        System.out.println("Address : Phoenix Lane");
+        System.out.println(" : Cross Road");
+        System.out.println(" : Mauritius");
+        System.out.println("Phone : 56745787");
+        System.out.println("Email : zz@mail.com");
     }
 }
